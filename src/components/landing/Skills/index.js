5cf8e5afb1460796b16d12b9c875185af502c612
RenanKanu <renankanu@gmail.com>
2020-06-02 13:32:13 -0300

    add section about me

--- src/components/landing/Skills/index.js
+++ src/components/landing/Skills/index.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-import { Container, Button } from 'components/common';
-import dev from 'assets/illustrations/skills.svg';
+import { Container } from '../../common/Button/index';
+import { Button } from '../../common/Button/index';
+import dev from '../../../assets/illustrations/skills.svg';
 import { Wrapper, SkillsWrapper, Details, Thumbnail } from './styles';
 
 export const Skills = () => (
   <Wrapper id="about">
     <SkillsWrapper as={Container}>
       <Thumbnail>
-        <img src={dev} alt="I’m John and I’m a Backend & Devops engineer!" />
+        <img src={dev} alt="I’m Renan" />
       </Thumbnail>
       <Details>
-        <h1>Hi There!</h1>
+        <h1>Um pouco sobre mim  </h1>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s
-          standard dummy.
+          Sobre mim texto
+        {/* Olá, meu nome é Renan Santos de Oliveira moro em Cianorte-PR, sou
+        formado em Análise e Desenvolvimento de Sistemas. Comecei na programação
+        em 2016 como desenvolvedor Android Nativo com Java, e atuava no
+        Back End em Spring Boot. No final do primeiro semestre de 2019 comecei
+        a programar com o PHP usando a arquitetura DDD, Android Nativo com
+        Kotlin, Docker e alguns serviços da AWS(ec2, SES, SNS, SQS, RDS,
+        Route53, Lambda). No segundo semestre de 2019 comecei a programar em
+        React Native e estou até o momento, estudando e lendo sobre algumas
+        tecnologias como: Flutter, NodeJs e me arriscando no Front End Web.
+        Nas horas vagas gosto de jogar Rugby. */}
         </p>
         <Button as={AnchorLink} href="#contact">
           Contato
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import { Helmet } from 'react-helmet'
 import { Layout } from '../components/common/Layout/index';
 import { Intro } from '../components/landing/Intro/index';
 import { Projects } from '../components/landing/Projects/index';
+import { Skills } from '../components/landing/Skills/index';
 
 export default () => {
   return (
     <Layout>
       <Intro />
+      <Skills />
       <Projects />
       <Helmet>
         <meta charSet="utf-8" />
