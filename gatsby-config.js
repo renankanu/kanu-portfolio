06b4148e2ba17ba9410926f65749ab2780ee3aa9
RenanKanu <renankanu@gmail.com>
2020-06-02 02:18:18 -0300

    Add .env for production

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,23 @@
-require("dotenv").config({
+require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
-})
+});
 
-export const plugins = [
-  'gatsby-plugin-react-helmet',
-  'gatsby-plugin-styled-components',
-  'gatsby-plugin-offline',
-  {
-    resolve: 'gatsby-source-graphql',
-    options: {
-      typeName: 'GitHub',
-      fieldName: 'github',
-      url: 'https://api.github.com/graphql',
-      headers: {
-        Authorization: `bearer ${process.env.GATSBY_PORTFOLIO_GITHUB_TOKEN}`,
+module.exports = {
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        typeName: 'GitHub',
+        fieldName: 'github',
+        url: 'https://api.github.com/graphql',
+        headers: {
+          Authorization: `bearer ${process.env.GATSBY_PORTFOLIO_GITHUB_TOKEN}`,
+        },
+        fetchOptions: {},
       },
-      fetchOptions: {},
     },
-  },
-]
+  ],
+}
