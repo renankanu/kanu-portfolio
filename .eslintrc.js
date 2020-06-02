e6557fa47d303cbb3fedec8bd93b6498470c1f8a
RenanKanu <renankanu@gmail.com>
2020-06-01 22:08:38 -0300

    Test deploy

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parse: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js']}
+    ]
+  },
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import React from "react"
+import React from 'react';
 
 export default function Home() {
-  return <div>Maíra Azevedo</div>
+  return <div>Renan Kanu</div>;
 }
