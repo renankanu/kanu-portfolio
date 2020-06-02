e33cb567585b3ecae0a7c3d1cf7133f380f9a43f
RenanKanu <renankanu@gmail.com>
2020-06-01 23:12:29 -0300

    Add components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Helmet } from 'react-helmet'
+import {Title, Wrapper} from './style'
+import GlobalStyle from './global-style'
 
 export default function Home() {
-  return <div>Renan Kanu</div>;
+  return (
+    <>
+      <GlobalStyle/>
+      <Wrapper>
+      <Title>Renan Kanu</Title>
+      </Wrapper>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{`<RenanKanu>`}</title>
+      </Helmet>
+    </>
+  )
 }
